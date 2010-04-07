a35bac3266c71cc8412fe49eabaffed1ea23bbc1
spint <stijn.pint@gmail.com>
2010-04-07 11:26:01 +0200

    twitter username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,17 @@ class User < ActiveRecord::Base
 
   acts_as_authentic
 
+  after_create :fill_twitter_username
+
   def twitter_profile
     @twitter_profile ||= JSON.parse(access_token.get('/account/verify_credentials.json').body)
+  end
+
+  def fill_twitter_username
+    if login.blank?
+      self.login = self.twitter_profile['screen_name']
+      save!
+    end
   end
 
 
