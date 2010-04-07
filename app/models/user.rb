efbadb77ed6147eb025a6795b057b8c8694625e8
spint <stijn.pint@gmail.com>
2010-04-07 11:35:47 +0200

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,16 @@ class User < ActiveRecord::Base
 
   acts_as_authentic
 
-  after_create :fill_twitter_username
+  before_create :fill_twitter_username
 
   def twitter_profile
     @twitter_profile ||= JSON.parse(access_token.get('/account/verify_credentials.json').body)
   end
 
   def fill_twitter_username
-#    if login.blank?
-#      self.login = self.twitter_profile['screen_name']
-#      save!
-#    end
-    puts twitter_profile
+    if login.blank?
+      self.login = self.twitter_profile['screen_name']
+    end
   end
 
 
